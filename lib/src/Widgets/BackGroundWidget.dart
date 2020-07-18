e967f6e1aab21cea5a899e23d7ed2df472dda700
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-18 16:14:06 -0400

    responsive

--- lib/src/Widgets/BackGroundWidget.dart
+++ lib/src/Widgets/BackGroundWidget.dart
@@ -6,19 +6,24 @@ class BackGroundWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double _sizeImage = 110.0;
     double _opacity = 0.3;
+
+    final size = MediaQuery.of(context).size;
+    final _width = size.width/100;
+    final _heigh = size.height/100;
+    final iconTam = _width*30;
+    double _sizeImage = iconTam;
     return Container(
       color: Color.fromRGBO(239,241,250, 1.0),
       child: Stack(
         children: [
-          Positioned(top: 20.0, left: 20.0,   width: _sizeImage , child: Image.asset("lib/src/Sources/Elements/Beverage.png",color: Colors.black.withOpacity(_opacity),)),
-          Positioned(top: 100.0, right: 30.0, width: _sizeImage , child: Image.asset("lib/src/Sources/Elements/Dessert.png",color: Colors.black.withOpacity(_opacity),)),
-          Positioned(top: 180.0, left: 20.0,  width: _sizeImage , child: Image.asset("lib/src/Sources/Elements/Maincourse.png",color: Colors.black.withOpacity(_opacity),)),
-          Positioned(top: 300.0, right: 20.0, width: _sizeImage , child: Image.asset("lib/src/Sources/Elements/Mexicanfood-1.png",color: Colors.black.withOpacity(_opacity),)),
-          Positioned(top: 400.0, left: 10.0,  width: _sizeImage-20 , child: Image.asset("lib/src/Sources/Elements/Salad.png",color: Colors.black.withOpacity(_opacity),)),
-          Positioned(top: 410.0, right: 10.0,  width: _sizeImage , child: Image.asset("lib/src/Sources/Elements/Burger.png",color: Colors.black.withOpacity(_opacity),)),
-          Positioned(top: 530.0, left: size.width/2 -55,  width: _sizeImage , child: Image.asset("lib/src/Sources/Elements/Mexicanfood-1.png",color: Colors.black.withOpacity(_opacity),)),
+          Positioned( top: _heigh,     left: _width,          height: _sizeImage,     width: _sizeImage ,    child: Image.asset("lib/src/Sources/Elements/Beverage.png",color: Colors.black.withOpacity(_opacity),     height: iconTam,width: iconTam,)),
+          Positioned( top: _heigh*10,  right: _width*5,       height: _sizeImage,     width: _sizeImage ,    child: Image.asset("lib/src/Sources/Elements/Dessert.png",color: Colors.black.withOpacity(_opacity),      height: iconTam,width: iconTam,)),
+          Positioned( top: _heigh*30,  left: _width,          height: _sizeImage,     width: _sizeImage ,    child: Image.asset("lib/src/Sources/Elements/Maincourse.png",color: Colors.black.withOpacity(_opacity),   height: iconTam,width: iconTam,)),
+          Positioned( top: _heigh*40,  right: _width,         height: _sizeImage,     width: _sizeImage ,    child: Image.asset("lib/src/Sources/Elements/Mexicanfood-1.png",color: Colors.black.withOpacity(_opacity),height: iconTam,width: iconTam,)),
+          Positioned( top: _heigh*60,  left: _width,          height: _sizeImage,     width: _sizeImage-20 , child: Image.asset("lib/src/Sources/Elements/Salad.png",color: Colors.black.withOpacity(_opacity),        height: iconTam,width: iconTam,)),
+          Positioned( top: _heigh*70,  right: _width,         height: _sizeImage,     width: _sizeImage ,    child: Image.asset("lib/src/Sources/Elements/Burger.png",color: Colors.black.withOpacity(_opacity),       height: iconTam,width: iconTam,)),
+          Positioned( top: _heigh*85,  left: _width*35,       height: _sizeImage,     width: _sizeImage ,    child: Image.asset("lib/src/Sources/Elements/Mexicanfood-1.png",color: Colors.black.withOpacity(_opacity),height: iconTam,width: iconTam,)),
         ],
       ),
     );
