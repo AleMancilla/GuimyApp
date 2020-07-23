69971c7e896354cf3b2819282e5ad9dff564d493
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-23 00:34:53 -0400

    PageStar

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:guimyapp/src/Pages/AuthUser/RegisterUser.dart';
 import 'package:guimyapp/src/Pages/SplashPage.dart';
 import 'package:guimyapp/src/PreferensUser/PreferenciasUsuario.dart';
 import 'package:guimyapp/src/Provider/ModelProvider.dart';
+import 'package:guimyapp/src/Provider/ModelTopRest.dart';
 import 'package:guimyapp/src/Templates/HomePage.dart';
 import 'package:guimyapp/src/root/root.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
@@ -41,6 +42,7 @@ class MyApp extends StatelessWidget {
       child: MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (context) => new ModelProvider()),
+          ChangeNotifierProvider(create: (context) => new ModelTopRest()),
         ],
         child: MaterialApp(
           title: 'Guimy',
